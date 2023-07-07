8d4ebcd8939ca57a6e92cdd68d528b6204cd5f1a
andrewmk <1872642+andrewmk@users.noreply.github.com>
2023-07-07 15:39:39 +0100

    Create HeatMap.py
    Produce a table of the number of solutions for each month/day combination.

---
+++ HeatMap.py
@@ -0,0 +1,9 @@
+import PuzzleADaySolver as p
+
+for month in range(1, 13):
+    for day in range(1, 32):
+        solver = p.PuzzleADaySolver(month, day, True)
+        solver.solve_board(solver.board, solver.piece_positions)
+        num_solutions = len(solver.solutions)
+        print(f"{num_solutions}, ", end='')
+    print("\n")
